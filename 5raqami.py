3039182ab7214002af4c36b130feade4050b53f6
hnwamini <162687502+hnwamini@users.noreply.github.com>
2024-05-14 21:38:57 +0330

    Add files via upload

---
+++ 5raqami.py
@@ -0,0 +1,15 @@
+def reverse_decimal_number(number):
+    if len(number) == 1:
+        return number
+    else:
+        return number [-1]  +
+reverse_decimal_number(number[:-1])
+#ورودی را دریافت کنید
+decimal_number = input("adad 5raqami ashari vared konid")
+
+while len(decimal_number) < 5:
+    decimal_number = input("vorodi bayd hadaghl 5raqami bashd,dobare adad vared konid")
+
+reversed_number =
+reverse_decimal_number(decimal_number)
+print("عدد وارونه:", reversed_number)
